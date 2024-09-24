753ed88f9e5bc0c95dcd84fcaeca1c106c8ad7bf
arymariano <ary.vm@hotmail.com>
2024-09-24 10:27:26 -0300

    desafio iphone

---
+++ src/Iphone.java
@@ -0,0 +1,48 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo pagina");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void ligar(String numero) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void atender() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void tocar() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void pausar() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        // TODO Auto-generated method stub
+    }
+    
+}
---
+++ src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+
+    public static void main(String[] args) {
+        
+        Iphone iphone = new Iphone();
+
+        iphone.adicionarNovaAba();
+
+    }
+
+}
